86067ebef5305136f97d4b3f25f8fbc1dd37e479
Erick <erick.munekata@gmail.com>
2020-07-08 21:49:18 -0300

    Mudanca no layout do header, estilizacao do botao de criar task e prototipacao dos cards de tasks

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 import { Link } from 'react-router-dom';
 import LogoImg from '../../assets/logo.svg';
-import { Header, Cards } from './styles';
+import {
+  Header,
+  Title,
+  Cards,
+  Task,
+  CardsToBeDone,
+  CardsInProgress,
+  CardsDone,
+} from './styles';
 
-import Card from '../../components/Card';
+// import Card from '../../components/Card';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <Header>
-        <img src={LogoImg} alt="TodoApp" />
-        <h1>Create and organize your daily tasks</h1>
+        <Title>
+          <img src={LogoImg} alt="TodoApp" />
+          <h1>Create and organize your daily tasks</h1>
+        </Title>
         <Link to="/Card">Criar Task</Link>
       </Header>
 
-      <Cards />
+      <Cards>
+        <CardsToBeDone>
+          <Task isWaiting inProgress={false} isDone={false}>
+            <span>Estudar React</span>
+            <span>2d 15h 45m 10s</span>
+          </Task>
+          <Task isWaiting inProgress={false} isDone={false}>
+            <span>Modelar Machine learning</span>
+            <span>12h 10m 02s</span>
+          </Task>
+          <Task isWaiting inProgress={false} isDone={false}>
+            <span>Concluir bootcamp</span>
+            <span>30d 15h 45m 10s</span>
+          </Task>
+        </CardsToBeDone>
+        <CardsInProgress>
+          <Task isWaiting={false} inProgress isDone={false}>
+            <span>Fazer front-end do app</span>
+          </Task>
+        </CardsInProgress>
+        <CardsDone>
+          <Task isWaiting={false} inProgress={false} isDone>
+            <span>Fazer café da tarde</span>
+          </Task>
+        </CardsDone>
+      </Cards>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,15 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface TaskProps {
+  isWaiting: boolean;
+  inProgress: boolean;
+  isDone: boolean;
+}
 
 export const Header = styled.header`
-  display: flex;
-  flex-direction: column;
-
-  h1 {
-    font-size: 32px;
-    color: #3a3a3a;
-    margin-top: 80px;
-  }
-
   a {
     display: flex;
     justify-content: center;
@@ -23,9 +20,74 @@ export const Header = styled.header`
     color: #5e5353;
     font: 16px Roboto, sans-serif;
     background-color: #1cff43;
+    box-shadow: 2px 3px 3px #009933;
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  h1 {
+    font-size: 32px;
+    color: #3a3a3a;
+    margin: 0 auto;
   }
 `;
 
 export const Cards = styled.div`
+  display: flex;
+  flex-direction: row;
   margin-top: 30px;
+  justify-content: space-between;
+
+  width: 100%;
 `;
+
+export const Task = styled.div<TaskProps>`
+  display: flex;
+  flex-direction: column;
+
+  width: 330px;
+  height: 100px;
+  flex: 1;
+
+  ${(props) =>
+    props.isWaiting &&
+    css`
+      border-color: blue;
+    `}
+
+  ${(props) =>
+    props.inProgress &&
+    css`
+      border-color: green;
+    `}
+
+  ${(props) =>
+    props.isDone &&
+    css`
+      border-color: yellow;
+    `}
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid black;
+  border-radius: 5px;
+
+  span {
+    padding: 6px 6px;
+  }
+`;
+
+export const CardsToBeDone = styled.div``;
+
+export const CardsInProgress = styled.div``;
+
+export const CardsDone = styled.div``;
